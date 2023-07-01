74b6d959c8d2a7578c5b30c3bced10bfe7f2af8c
HarshJa1n <hjain012003@gmail.com>
2023-07-01 19:02:46 +0530

    [fix] Optional headings for res

--- pages/resources/[resource].tsx
+++ pages/resources/[resource].tsx
@@ -1,58 +1,3 @@
-// import React from 'react';
-// import { Card, ConfigProvider, theme, Tag } from 'antd';
-// import { useTheme } from 'next-themes';
-// import resourcesData from '../../components/data/resourceData';
-// import Link from 'next/link';
-// import { useRouter } from 'next/router';
-// const { Meta } = Card;
-
-// const ResourcePage = () => {
-//     const router = useRouter();
-//     const { resource } = router.query;
-//     console.log("id", resource)
-//     const { resolvedTheme, setTheme } = useTheme();
-//     const { defaultAlgorithm, darkAlgorithm } = theme;
-
-//     const resourceD = resourcesData?.find((topic) => topic.id === parseInt(resource as string));
-
-//     if (!resourceD) {
-//         return <div>Resource not found.</div>;
-//     }
-
-//     return (
-//         <div>
-//             <h1>{resourceD.title}</h1>
-//             <ConfigProvider
-//                 theme={{
-//                     algorithm: resolvedTheme === 'dark' ? darkAlgorithm : defaultAlgorithm,
-//                 }}
-//             >
-//                 {resourceD?.subtopics?.map((subtopic) => (
-//                     <Card key={subtopic.id} style={{ marginBottom: '10px' }}>
-//                         <Meta
-//                             title={
-//                                 <Link href={`/resources/${resourceD.id}/${subtopic.id}`}>
-//                                     <a>{subtopic.title}</a>
-//                                 </Link>
-//                             }
-//                         />
-//                         <div style={{ marginTop: '10px' }}>
-//                             {/* {subtopic?.url.map((link) => (
-//                                 <div key={link.id}>
-//                                     <Link href={`/resources/${resourceD.id}/${subtopic.id}/${link.id}`}>
-//                                         <a>{link.title}</a>
-//                                     </Link>
-//                                 </div>
-//                             ))} */}
-//                         </div>
-//                     </Card>
-//                 ))}
-//             </ConfigProvider>
-//         </div>
-//     );
-// };
-
-// export default ResourcePage;
 import React from 'react';
 import { List, ConfigProvider, theme, Divider } from 'antd';
 import { useTheme } from 'next-themes';
@@ -60,15 +5,12 @@ import resourcesData from '../../components/data/resourceData';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-
-
 const ResourcePage = () => {
     const router = useRouter();
     const { resource } = router.query;
     console.log("id", resource)
     const { resolvedTheme, setTheme } = useTheme();
     const { defaultAlgorithm, darkAlgorithm } = theme;
-
 
     const resourceD = resourcesData?.find((topic) => topic.id === parseInt(resource as string));
 
@@ -84,51 +26,58 @@ const ResourcePage = () => {
                     algorithm: resolvedTheme === 'dark' ? darkAlgorithm : defaultAlgorithm,
                 }}
             >
-                <List
-                    dataSource={resourceD.links}
-                    renderItem={(link) => (
-                        <List.Item>
-                            <a href={link.url} target='blank'>{link.title}</a>
-                        </List.Item>
-                    )}
-                />
-                <Divider />
-                <h1>Subtopics</h1>
-                <List
-                    dataSource={resourceD.subtopics}
-                    renderItem={(subtopic) => (
-                        <List.Item>
-                            <h2>{subtopic.title}:</h2>
+                {resourceD.links.length > 0 && (
+                    <>
+                        <List
+                            dataSource={resourceD.links}
+                            renderItem={(link) => (
+                                <List.Item>
+                                    <a href={link.url} target='blank'>{link.title}</a>
+                                </List.Item>
+                            )}
+                        />
+                        <Divider />
+                    </>
+                )}
 
-                            <List
-                                dataSource={subtopic.subLinks}
-                                renderItem={(subtopic) => (
-                                    <List.Item>
+                {resourceD.subtopics.length > 0 && (
+                    <>
+                        <h1>Subtopics</h1>
+                        <List
+                            dataSource={resourceD.subtopics}
+                            renderItem={(subtopic) => (
+                                <List.Item>
+                                    <h2>{subtopic.title}:</h2>
+                                    <List
+                                        dataSource={subtopic.subLinks}
+                                        renderItem={(subLink) => (
+                                            <List.Item>
+                                                <a href={subLink}>{subLink}</a>
+                                            </List.Item>
+                                        )}
+                                        className='ml-5'
+                                    />
+                                </List.Item>
+                            )}
+                        />
+                        <Divider />
+                    </>
+                )}
 
-                                        <a href={subtopic}>{subtopic}</a>
-
-                                    </List.Item>
-                                )}
-                                className='ml-5'
-                            />
-                        </List.Item>
-                    )}
-                />
-                <Divider />
-                <h1>Moreover...</h1>
-                <List
-                    dataSource={resourceD.URLs}
-                    renderItem={(url) => (
-                        <List.Item>
-
-                            <a href={url} target='blank'>{url}</a>
-
-
-                        </List.Item>
-                    )}
-                />
-                <Divider />
-
+                {resourceD.URLs.length > 0 && (
+                    <>
+                        <h1>Moreover...</h1>
+                        <List
+                            dataSource={resourceD.URLs}
+                            renderItem={(url) => (
+                                <List.Item>
+                                    <a href={url} target='blank'>{url}</a>
+                                </List.Item>
+                            )}
+                        />
+                        <Divider />
+                    </>
+                )}
             </ConfigProvider>
         </div>
     );
